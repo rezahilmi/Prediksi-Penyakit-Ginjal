d88d619b7caee5f2b69b7d909afd8d239eccb6e4
Reza Hilmi Dafa <rezahilmidafa@gmail.com>
2024-12-09 22:35:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ Tabs = {
 # Sidebar navigation
 with st.sidebar:
     selected = option_menu("Navigasi", ["Home", "Prediksi", "Visualisasi"],
-                           icons=["house", "gear", "diagram-3"], menu_icon="cast", default_index=0)
+                           icons=["house", "robot", "diagram-3"], menu_icon="cast", default_index=0)
 
 
 # Load dataset
