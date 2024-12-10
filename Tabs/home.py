d23854e303a2f2c19d026fc364e473afe6a845bb
Reza Hilmi Dafa <rezahilmidafa@gmail.com>
2024-12-10 22:06:33 +0700

    update

--- Tabs/home.py
+++ Tabs/home.py
@@ -4,3 +4,17 @@ import streamlit as st
 def app():
     # judul aplikasi
     st.title("Aplikasi Prediksi Penyakit Batu Ginjal")
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.header("")
+        st.title(''':blue[Early Detection, Better Protection]''')
+        st.text("")
+        st.text("Kelompok 9 - Data Mining 2024")
+
+    with col2:
+        st.image("Tabs/gambar_ginjal.png")
+
+    st.title("")
+    st.markdown(
+        "Aplikasi ini dikembangkan menggunakan **Streamlit** untuk memprediksi risiko penyakit batu ginjal berdasarkan data medis pasien. Dengan memanfaatkan algoritma :blue-background[Decision Tree], aplikasi ini memberikan hasil prediksi secara interaktif dan menampilkan visualisasi performa model.")
--- main.py
+++ main.py
@@ -11,6 +11,28 @@ Tabs = {
     "Visualisasi": visualise
 }
 
+
+def footer():
+    footer = """
+    <style>
+        .footer {
+            position: fixed;
+            left: 300px;
+            bottom: 0;
+            width: 100%;
+            background-color:;
+            color: var(--text-color);
+            text-align: left;
+            padding: 10px;
+        }
+    </style>
+    <div class="footer">
+        <p>Kelompok 9 - Data Mining 2024</p>
+    </div>
+    """
+    st.markdown(footer, unsafe_allow_html=True)
+
+
 # Sidebar navigation
 with st.sidebar:
     selected = option_menu("Navigasi", ["Home", "Prediksi", "Visualisasi"],
